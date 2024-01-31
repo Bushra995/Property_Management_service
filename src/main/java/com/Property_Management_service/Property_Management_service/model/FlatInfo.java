442d67af57b4cf22b796db489c3b12e75d88bfb2
Bushra995 <bushraf995@gmail.com>
2024-02-01 02:37:03 +0530

    Refactor: FlatInfo model

--- src/main/java/com/Property_Management_service/Property_Management_service/model/FlatInfo.java
+++ src/main/java/com/Property_Management_service/Property_Management_service/model/FlatInfo.java
@@ -6,7 +6,9 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
-    @Entity
+import java.util.List;
+
+@Entity
     @Getter
     @Setter
     @AllArgsConstructor
@@ -17,26 +19,33 @@ import lombok.Setter;
         @Id
         @GeneratedValue(strategy = GenerationType.IDENTITY)
         private Long id;
+
         private String type;
         private String Location;
-        private Integer flatTitle;
-        private String bhkType;
-        private Double gMapLocation;
-        private String area;
+        private String flatTitle;
+        private Integer bhkType;
+        private String gMapLocation;
+        private Double area;
         private String manualAddress;
         private Integer pinCode;
         private String isFurnished;
         private Double price;
         private Boolean isConstructed;
         private Integer parking;
-        private Integer availableIndicator;
+        private Integer avaiableIndicator;
         private Integer floor;
-        private String hasBalcony;
+        private Boolean hasBalcony;
         private String description;
         private Boolean underConstruction;
         private Integer propAge;
         private Integer noOfBathroom;
         private Integer propFloor;
         private String propType;
+
+        @OneToMany(mappedBy = "flatInfo", cascade = CascadeType.ALL)
+        private List<Amenities> amenities;
+
+        @OneToMany(mappedBy = "flatInfo", cascade = CascadeType.ALL)
+        private List<Images> images;
     }
 
